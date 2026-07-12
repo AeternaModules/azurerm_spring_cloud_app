d755031d2032a2beb91dd7d532551783505f13f0
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:20:32 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,61 +1,57 @@
-output "spring_cloud_apps" {
-  description = "All spring_cloud_app resources"
-  value       = azurerm_spring_cloud_app.spring_cloud_apps
-}
 output "spring_cloud_apps_addon_json" {
-  description = "List of addon_json values across all spring_cloud_apps"
-  value       = [for k, v in azurerm_spring_cloud_app.spring_cloud_apps : v.addon_json]
+  description = "Map of addon_json values across all spring_cloud_apps, keyed the same as var.spring_cloud_apps"
+  value       = { for k, v in azurerm_spring_cloud_app.spring_cloud_apps : k => v.addon_json }
 }
 output "spring_cloud_apps_custom_persistent_disk" {
-  description = "List of custom_persistent_disk values across all spring_cloud_apps"
-  value       = [for k, v in azurerm_spring_cloud_app.spring_cloud_apps : v.custom_persistent_disk]
+  description = "Map of custom_persistent_disk values across all spring_cloud_apps, keyed the same as var.spring_cloud_apps"
+  value       = { for k, v in azurerm_spring_cloud_app.spring_cloud_apps : k => v.custom_persistent_disk }
 }
 output "spring_cloud_apps_fqdn" {
-  description = "List of fqdn values across all spring_cloud_apps"
-  value       = [for k, v in azurerm_spring_cloud_app.spring_cloud_apps : v.fqdn]
+  description = "Map of fqdn values across all spring_cloud_apps, keyed the same as var.spring_cloud_apps"
+  value       = { for k, v in azurerm_spring_cloud_app.spring_cloud_apps : k => v.fqdn }
 }
 output "spring_cloud_apps_https_only" {
-  description = "List of https_only values across all spring_cloud_apps"
-  value       = [for k, v in azurerm_spring_cloud_app.spring_cloud_apps : v.https_only]
+  description = "Map of https_only values across all spring_cloud_apps, keyed the same as var.spring_cloud_apps"
+  value       = { for k, v in azurerm_spring_cloud_app.spring_cloud_apps : k => v.https_only }
 }
 output "spring_cloud_apps_identity" {
-  description = "List of identity values across all spring_cloud_apps"
-  value       = [for k, v in azurerm_spring_cloud_app.spring_cloud_apps : v.identity]
+  description = "Map of identity values across all spring_cloud_apps, keyed the same as var.spring_cloud_apps"
+  value       = { for k, v in azurerm_spring_cloud_app.spring_cloud_apps : k => v.identity }
 }
 output "spring_cloud_apps_ingress_settings" {
-  description = "List of ingress_settings values across all spring_cloud_apps"
-  value       = [for k, v in azurerm_spring_cloud_app.spring_cloud_apps : v.ingress_settings]
+  description = "Map of ingress_settings values across all spring_cloud_apps, keyed the same as var.spring_cloud_apps"
+  value       = { for k, v in azurerm_spring_cloud_app.spring_cloud_apps : k => v.ingress_settings }
 }
 output "spring_cloud_apps_is_public" {
-  description = "List of is_public values across all spring_cloud_apps"
-  value       = [for k, v in azurerm_spring_cloud_app.spring_cloud_apps : v.is_public]
+  description = "Map of is_public values across all spring_cloud_apps, keyed the same as var.spring_cloud_apps"
+  value       = { for k, v in azurerm_spring_cloud_app.spring_cloud_apps : k => v.is_public }
 }
 output "spring_cloud_apps_name" {
-  description = "List of name values across all spring_cloud_apps"
-  value       = [for k, v in azurerm_spring_cloud_app.spring_cloud_apps : v.name]
+  description = "Map of name values across all spring_cloud_apps, keyed the same as var.spring_cloud_apps"
+  value       = { for k, v in azurerm_spring_cloud_app.spring_cloud_apps : k => v.name }
 }
 output "spring_cloud_apps_persistent_disk" {
-  description = "List of persistent_disk values across all spring_cloud_apps"
-  value       = [for k, v in azurerm_spring_cloud_app.spring_cloud_apps : v.persistent_disk]
+  description = "Map of persistent_disk values across all spring_cloud_apps, keyed the same as var.spring_cloud_apps"
+  value       = { for k, v in azurerm_spring_cloud_app.spring_cloud_apps : k => v.persistent_disk }
 }
 output "spring_cloud_apps_public_endpoint_enabled" {
-  description = "List of public_endpoint_enabled values across all spring_cloud_apps"
-  value       = [for k, v in azurerm_spring_cloud_app.spring_cloud_apps : v.public_endpoint_enabled]
+  description = "Map of public_endpoint_enabled values across all spring_cloud_apps, keyed the same as var.spring_cloud_apps"
+  value       = { for k, v in azurerm_spring_cloud_app.spring_cloud_apps : k => v.public_endpoint_enabled }
 }
 output "spring_cloud_apps_resource_group_name" {
-  description = "List of resource_group_name values across all spring_cloud_apps"
-  value       = [for k, v in azurerm_spring_cloud_app.spring_cloud_apps : v.resource_group_name]
+  description = "Map of resource_group_name values across all spring_cloud_apps, keyed the same as var.spring_cloud_apps"
+  value       = { for k, v in azurerm_spring_cloud_app.spring_cloud_apps : k => v.resource_group_name }
 }
 output "spring_cloud_apps_service_name" {
-  description = "List of service_name values across all spring_cloud_apps"
-  value       = [for k, v in azurerm_spring_cloud_app.spring_cloud_apps : v.service_name]
+  description = "Map of service_name values across all spring_cloud_apps, keyed the same as var.spring_cloud_apps"
+  value       = { for k, v in azurerm_spring_cloud_app.spring_cloud_apps : k => v.service_name }
 }
 output "spring_cloud_apps_tls_enabled" {
-  description = "List of tls_enabled values across all spring_cloud_apps"
-  value       = [for k, v in azurerm_spring_cloud_app.spring_cloud_apps : v.tls_enabled]
+  description = "Map of tls_enabled values across all spring_cloud_apps, keyed the same as var.spring_cloud_apps"
+  value       = { for k, v in azurerm_spring_cloud_app.spring_cloud_apps : k => v.tls_enabled }
 }
 output "spring_cloud_apps_url" {
-  description = "List of url values across all spring_cloud_apps"
-  value       = [for k, v in azurerm_spring_cloud_app.spring_cloud_apps : v.url]
+  description = "Map of url values across all spring_cloud_apps, keyed the same as var.spring_cloud_apps"
+  value       = { for k, v in azurerm_spring_cloud_app.spring_cloud_apps : k => v.url }
 }
 
