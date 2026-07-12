93b637b49dbe827d12ab39718b9034c3c18b974b
Daniel Allen <dan@aeternagroup.net>
2026-07-12 16:05:03 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "spring_cloud_apps_id" {
+  description = "Map of id values across all spring_cloud_apps, keyed the same as var.spring_cloud_apps"
+  value       = { for k, v in azurerm_spring_cloud_app.spring_cloud_apps : k => v.id }
+}
 output "spring_cloud_apps_addon_json" {
   description = "Map of addon_json values across all spring_cloud_apps, keyed the same as var.spring_cloud_apps"
   value       = { for k, v in azurerm_spring_cloud_app.spring_cloud_apps : k => v.addon_json }
